9007ef3244242dbc92b3eb980bc782cc7e6fcd36
YokimSl <jhonnata1337@gmail.com>
2022-02-09 17:53:53 -0300

    segund commit

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,15 +1,36 @@
 import React from "react";
+import { HighlightCard } from "../../HighlightCard";
+
 
 import { 
     Container,
-    Title
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
        } from "./styles";
 export function Dashboard(){
     return(
-        <Container>
-            <Title>
-                DashBoard
-            </Title>
-        </Container>
+    <Container>
+        <Header>
+         <UserWrapper>
+            <UserInfo>
+                <Photo 
+                source={{ uri: 'https://avatars.githubusercontent.com/u/49276851?v=4'}}
+                />
+                    <User>
+                        <UserGreeting>Olá,</UserGreeting>
+                        <UserName>Jhonnata</UserName>
+                    </User>
+            </UserInfo>
+            <Icon name="power"/>
+         </UserWrapper>
+        </Header>
+        <HighlightCard />
+    </Container>
     );
 }
